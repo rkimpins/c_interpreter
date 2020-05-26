c6930834bb9773ee9b97affcca8b907d1ddd79cd
rkimpins <rkimpins@ualberta.ca>
2020-05-26 16:39:51 -0600

    Can now run basic commands and imports. Multiline functions are not yet supported

--- c_interpreter.cpp
+++ c_interpreter.cpp
@@ -1,6 +1,6 @@
 #include <stdio.h>
 using namespace std;
 int main() {
-printf("success\n");
+int x = 10;printf("%d", x);
 return 0;
 }
--- run.py
+++ run.py
@@ -14,7 +14,11 @@ class CCode:
         self.__includes = ["#include <stdio.h>"]
         self.__functions = []
         self.__variables = []
-        self.compiler = "g++" #"gcc", clang, clang++
+        self.__commands = []
+        self.__compiler = "g++" #"gcc", clang, clang++
+
+        self.insertion_order = []
+        self.__last_print = ""
 
     def compile(self):
         # Write to file, run file
@@ -24,29 +28,91 @@ class CCode:
             for function in self.__functions:
                 fp.write(function)
             fp.write("using namespace std;\n")
-            fp.write('int main() {\nprintf("success\\n");\nreturn 0;\n}') #need double \\ for prints
+            fp.write("int main() {\n")
+            for command in self.__commands:
+                fp.write(command)
+            fp.write(self.__last_print+"\n")
+            fp.write("return 0;\n}")
+
         # Compile
-        subprocess.call([self.compiler, self.filename])
+        subprocess.run([self.__compiler, self.filename])
+
         # Run
         tmp=subprocess.call("./a.out")
+        print()
+        #print(f"tmp: {tmp}")
+
+    def add_include(self, include):
+        self.__includes.append(include)
+        self.insertion_order.append("include")
+        self.__last_print = ""
+
+    def add_command(self, command):
+        self.__commands.append(command)
+        self.insertion_order.append("command")
+        self.__last_print = ""
+
+    def add_function(self, function):
+        self.__functions.append(function)
+        self.insertion_order.append("function")
+        self.__last_print = ""
+
+    def undo(self):
+        if len(self.insertion_order) == 0:
+            print("Nothing to undo")
+        else:
+            last_insertion = self.insertion_order.pop()
+            if last_insertion == "include":
+                self.__includes.pop()
+            elif last_insertion == "function":
+                self.__functions.pop()
+            elif last_insertion == "command":
+                self.__commands.pop()
+        self.__last_print = ""
+
+
+    def add_print(self, print_stmt):
+        self.__last_print = print_stmt
+
 
     def print_file(self):
         print(f"Contents of {self.filename}")
-        print("-"*10)
+        line_length = 20
+        print("-"*line_length)
         os.system(f"cat {self.filename}")
-        print("\n"+"-"*10)
+        print("\n"+"-"*line_length)
 
 def main():
     Runner = CCode()
     while True:
-        inp = input(">>>")
-
-
-
+        inp = input(">>> ")
+        if inp == "exit()":
+            break
+        elif inp in ["help", "h"]:
+            #TODO add help menu
+            print("help, h, exit(), print file, etc, can't redeclare variables")
+        elif inp == "undo":
+            Runner.undo()
+        elif inp == "file":
+            Runner.print_file()
+        elif inp == "":
+            pass
+        #elif inp[-1] == ";":
+        #    pass
+        elif inp[:8] == "#include":
+            Runner.add_include(inp)
+        elif inp[:4] == "cout" or inp[:9] == "std::cout" or inp[:5] == "print" or inp[:9] == "std::print":
+            #TODO remove using namespace from code?
+            Runner.add_print(inp)
+        else:
+            Runner.add_command(inp)
 
         Runner.compile()
+
         #TODO make debugger only
-        Runner.print_file()
+        #Runner.print_file()
+        #gcc -E file
+        #cpp file
 
 
 if __name__ == "__main__":
